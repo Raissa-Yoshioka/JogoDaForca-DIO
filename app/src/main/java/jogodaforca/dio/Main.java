6d793a825864d8323f4ef6872a52248ee8643d51
Raissa Rinaldi Yoshioka <rryoshioka@gmail.com>
2025-08-05 13:10:55 -0400

    Implementation of the menu on Main and renaming variables on models

--- app/src/main/java/jogodaforca/dio/Main.java
+++ app/src/main/java/jogodaforca/dio/Main.java
@@ -1,18 +1,51 @@
 package jogodaforca.dio;
 
+import java.util.Scanner;
 import java.util.stream.Stream;
 
+import jogodaforca.dio.exceptions.JogoFinalizadoException;
+import jogodaforca.dio.exceptions.LetraJaEscolhidaException;
 import jogodaforca.dio.models.JogoDaForca;
 import jogodaforca.dio.models.JogoDaForcaCaractere;
 
 public class Main {
 
-    public static void main(String... args) {
+    static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         System.out.println("\n");        
         var letra = Stream.of(args)
                     .map(a -> a.toLowerCase().charAt(0))
                     .map(JogoDaForcaCaractere::new).toList();
-        System.out.println(letra);
-        System.out.println(new JogoDaForca(letra));
+        var jogoDaForca = new JogoDaForca(letra);
+        System.out.println("Bem-vindo ao Jogo Da Forca, tente adivinhar a palavra, boa sorte.");
+        var option = -1;
+        do {
+            System.out.println(jogoDaForca);
+            System.out.println("Selecione uma das opções:");
+            System.out.println("1 - Verificar uma letra.");
+            System.out.println("2 - Verificar o status do jogo.");
+            System.out.println("3 - Sair do jogo");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> addCaractere(jogoDaForca);
+                case 2 -> System.out.println(jogoDaForca.getStatusDoJogo());
+                case 3 -> System.exit(0);
+                default-> System.out.println("Opção inválida");
+            }
+        } while (true);
+    }
+
+    private static void addCaractere(JogoDaForca jogoDaForca) {
+        System.out.println("Informe uma letra");
+        var caractere = scanner.next().toLowerCase().charAt(0);
+        try {
+            jogoDaForca.adicionarCaractere(caractere);
+        } catch (LetraJaEscolhidaException ex) {
+            System.out.println(ex.getMessage());
+        } catch (JogoFinalizadoException ex) {
+            System.out.println(ex.getMessage());
+            System.exit(0);
+        }
     }
 }
--- app/src/main/java/jogodaforca/dio/models/JogoDaForca.java
+++ app/src/main/java/jogodaforca/dio/models/JogoDaForca.java
@@ -1,54 +1,68 @@
 package jogodaforca.dio.models;
 
-import static jogodaforca.dio.models.JogoDaForcaStatus.DERROTA;
-import static jogodaforca.dio.models.JogoDaForcaStatus.PENDENTE;
-import static jogodaforca.dio.models.JogoDaForcaStatus.VITORIA;
+import static jogodaforca.dio.models.JogoDaForcaStatus.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 import jogodaforca.dio.exceptions.JogoFinalizadoException;
+import jogodaforca.dio.exceptions.LetraJaEscolhidaException;
 
 public class JogoDaForca {
 
-    private final int TAMANHO_LINHA_INICIAL = 9;
-    private final int TAMANHO_LINHA_INICIAL_COM_SEPARADOR = 10;
+    private final static int TAMANHO_INICIAL_LINHA = 9;
 
-    private final int tamanhoDaLinha;
-    private final int tamanhoInicialDaForca;
-    private final List<JogoDaForcaCaractere> bonecoDaForca;
+    private final int tamanhoLinha;
+    private final int tamanhoInicialForca;
+    private final List<JogoDaForcaCaractere> bonecoForca;
     private final List<JogoDaForcaCaractere> caracteres;
     private final List<Character> tentativasFalhas = new ArrayList<>();
 
     private String forca;
-    private JogoDaForcaStatus statusDoJogo;
+    private JogoDaForcaStatus statusJogo;
+
+    public JogoDaForcaStatus getStatusDoJogo() {
+        return statusJogo;
+    }
 
     public JogoDaForca(final List<JogoDaForcaCaractere> caracteres) {
         var espacoEmBranco = " ".repeat(caracteres.size());
-        var caractereDeEspaco = "-".repeat(caracteres.size());
-        this.statusDoJogo = PENDENTE;
-        this.bonecoDaForca = construirPosicoesBonecoDaForca();
-        this.tamanhoDaLinha = TAMANHO_LINHA_INICIAL_COM_SEPARADOR + espacoEmBranco.length();
-        criarDesignForca(espacoEmBranco, caractereDeEspaco);
-        this.caracteres = colocarEspacosDasLetrasNoJogo(caracteres, espacoEmBranco.length());
-        this.tamanhoInicialDaForca = forca.length();
+        var caracteresDeEspaco = "-".repeat(caracteres.size());
+        this.tamanhoLinha = TAMANHO_INICIAL_LINHA + espacoEmBranco.length() + System.lineSeparator().length();
+        this.statusJogo = PENDENTE;
+        this.bonecoForca = construirBonecoForca();
+        construirForca(espacoEmBranco, caracteresDeEspaco);
+        this.caracteres = colocarCaracteresEspacoNoJogo(caracteres);
+        this.tamanhoInicialForca = forca.length();
     }
 
     public void adicionarCaractere(final char caractere) {
-        if (this.statusDoJogo != PENDENTE) {
-            var mensagem = this.statusDoJogo == VITORIA ?
-            "Parabéns, você ganhou!" : "Você perdeu, tente novamente";
+        if (this.statusJogo != PENDENTE) {
+            var mensagem = this.statusJogo == VITORIA ?
+                    "Parabéns! Você ganhou o jogo" :
+                    "Você perdeu! Tente novamente";
             throw new JogoFinalizadoException(mensagem);
         }
-        var found = this.caracteres.stream().filter(c -> c.getCaractere() == caractere).toList();
-        if (found.isEmpty()) {
+
+        var found = this.caracteres.stream()
+                .filter(c -> c.getCaractere() == caractere)
+                .toList();
+
+        if (this.tentativasFalhas.contains(caractere)) {
+            throw new LetraJaEscolhidaException("A letra '" + caractere + "' já foi informada.");
+        }
+
+        if (found.isEmpty()){
             tentativasFalhas.add(caractere);
-            if (tentativasFalhas.size() >= 6) {
-                this.statusDoJogo = DERROTA;
+            if (tentativasFalhas.size() >= 6){
+                this.statusJogo = DERROTA;
             }
-            reconstruirForca(this.bonecoDaForca.removeFirst());
+            reconstruirForca(this.bonecoForca.removeFirst());
             return;
+        }
+        if (found.getFirst().isEhVisivel()){
+            throw new LetraJaEscolhidaException("A letra '" + caractere + "' já foi informada.");
         }
 
         this.caracteres.forEach(c -> {
@@ -57,60 +71,59 @@ public class JogoDaForca {
             }
         });
 
-        if (this.caracteres.stream().noneMatch(JogoDaForcaCaractere::isInvisivel)) {
-            this.statusDoJogo = VITORIA;
+        if (this.caracteres.stream().noneMatch(JogoDaForcaCaractere::isInvisivel)){
+            this.statusJogo = VITORIA;
         }
+
         reconstruirForca(found.toArray(JogoDaForcaCaractere[]::new));
     }
 
     @Override
     public String toString() {
-        return forca;
+        return this.forca;
     }
 
-    private List<JogoDaForcaCaractere> construirPosicoesBonecoDaForca() {
+    private void reconstruirForca(final JogoDaForcaCaractere... JogoDaForcaCaracteres){
+        var construtorForca = new StringBuilder(this.forca);
+        Stream.of(JogoDaForcaCaracteres).forEach(
+                h -> construtorForca.setCharAt(h.getPosicao(), h.getCaractere()
+                ));
+        var mensagemFalha = this.tentativasFalhas.isEmpty() ? "" : " Tentativas: " + this.tentativasFalhas;
+        this.forca = construtorForca.substring(0, tamanhoInicialForca) + mensagemFalha;
+    }
+
+    private List<JogoDaForcaCaractere> construirBonecoForca(){
         final var LINHA_DA_CABECA = 3;
         final var LINHA_DO_CORPO = 4;
-        final var LINHA_DAS_PERNAS = 5;
+        final var LINHA_DA_PERNA = 5;
         return new ArrayList<>(
-            List.of(
-                new JogoDaForcaCaractere('O', this.tamanhoDaLinha * LINHA_DA_CABECA + 6),
-                new JogoDaForcaCaractere('|', this.tamanhoDaLinha * LINHA_DO_CORPO + 6),
-                new JogoDaForcaCaractere('/', this.tamanhoDaLinha * LINHA_DO_CORPO + 5),
-                new JogoDaForcaCaractere('\\', this.tamanhoDaLinha * LINHA_DO_CORPO + 7),
-                new JogoDaForcaCaractere('/', this.tamanhoDaLinha * LINHA_DAS_PERNAS + 5),
-                new JogoDaForcaCaractere('\\', this.tamanhoDaLinha * LINHA_DAS_PERNAS + 7)
-            )
+                List.of(
+                        new JogoDaForcaCaractere('O', this.tamanhoLinha * LINHA_DA_CABECA + 6),
+                        new JogoDaForcaCaractere('|', this.tamanhoLinha * LINHA_DO_CORPO + 6),
+                        new JogoDaForcaCaractere('/', this.tamanhoLinha * LINHA_DO_CORPO + 5),
+                        new JogoDaForcaCaractere('\\', this.tamanhoLinha * LINHA_DO_CORPO + 7),
+                        new JogoDaForcaCaractere('/', this.tamanhoLinha * LINHA_DA_PERNA + 5),
+                        new JogoDaForcaCaractere('\\', this.tamanhoLinha * LINHA_DA_PERNA + 7)
+                )
         );
     }
 
-    private List<JogoDaForcaCaractere> colocarEspacosDasLetrasNoJogo(final List<JogoDaForcaCaractere> caracteres, final int QttEspacoEmBranco) {
-        final var LINHA_DE_LETRAS = 6;
-
-        for(int i = 0; i < caracteres.size(); i++) {
-            caracteres.get(i).setPosicao(this.tamanhoDaLinha * LINHA_DE_LETRAS + TAMANHO_LINHA_INICIAL + i);
+    private List<JogoDaForcaCaractere> colocarCaracteresEspacoNoJogo(final List<JogoDaForcaCaractere> caracteres){
+        final var LINHA_DAS_LETRAS = 7;
+        for (int i = 0; i < caracteres.size(); i++) {
+            caracteres.get(i).setPosicao(this.tamanhoLinha * LINHA_DAS_LETRAS + TAMANHO_INICIAL_LINHA + i);
         }
-
         return caracteres;
     }
 
-    private void reconstruirForca(final JogoDaForcaCaractere... caracteresForca) {
-        var construtorForca = new StringBuilder(this.forca);
-        Stream.of(caracteresForca).forEach(h -> construtorForca.setCharAt(h.getPosicao(), h.getCaractere()));
-
-        var mensagemFalha = this.tentativasFalhas.isEmpty() ? "" : "Tentativas " + this.tentativasFalhas;
-        this.forca = construtorForca.substring(0, tamanhoInicialDaForca) + mensagemFalha;
-    }
-
-    private void criarDesignForca(final String espacoEmBranco, final String caractereDeEspaco) {
-        this.forca = "  _____  " + espacoEmBranco + System.lineSeparator() + 
-                     "  |   |  " + espacoEmBranco + System.lineSeparator() +
-                     "  |   |  " + espacoEmBranco + System.lineSeparator() +
-                     "  |      " + espacoEmBranco + System.lineSeparator() +
-                     "  |      " + espacoEmBranco + System.lineSeparator() +
-                     "  |      " + espacoEmBranco + System.lineSeparator() +
-                     "  |      " + espacoEmBranco + System.lineSeparator() +
-                     "  |      " + espacoEmBranco + System.lineSeparator() +
-                     "=========" + caractereDeEspaco + System.lineSeparator();
+    private void construirForca(final String espacoEmBranco, final String caracteresDeEspaco){
+        this.forca = "  -----  " + espacoEmBranco + System.lineSeparator() +
+                       "  |   |  " + espacoEmBranco + System.lineSeparator() +
+                       "  |   |  " + espacoEmBranco + System.lineSeparator() +
+                       "  |      " + espacoEmBranco + System.lineSeparator() +
+                       "  |      " + espacoEmBranco + System.lineSeparator() +
+                       "  |      " + espacoEmBranco + System.lineSeparator() +
+                       "  |      " + espacoEmBranco + System.lineSeparator() +
+                       "=========" + caracteresDeEspaco + System.lineSeparator();
     }
 }
